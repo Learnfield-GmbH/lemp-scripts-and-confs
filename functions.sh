ea9b1860722cfa99b0e3f485fba49664caaa56b2
Ivo van Beek <ivodvb@users.noreply.github.com>
2017-08-16 11:27:36 +0200

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+isUbuntu() {
+    dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
+
+    if [ "$dist" = "Ubuntu" ]; then
+        return true
+    fi
+
+    return false
+}
+
+# Checks if a user belongs to a group
+inGroup(){
+   group="$1"
+   user="${2:-$(whoami)}"
+   ret=false
+   for x in $(groups "$user" |sed "s/.*://g")
+   do [[ "$x" == "$group" ]] && { ret=true ; break ; }
+   done
+   eval "${ret}"
+}
+
+# Prints a message and logs it
+printAndLog() {
+    echo "$1"
+    echo "$1" >> /tmp/provision.log
+    echo "$1" >> /tmp/provision_verbose.log
+}
+
+# Tries to execute a command and if it fails it waits for five seconds and retries is for max 100 times
+function tryCommand() {
+    printAndLog "Trying to $1"
+
+    if [ -n "$2" ]; then
+        TRIES=$2
+    else
+        TRIES=1
+    fi
+
+    if (( TRIES > 100 )); then
+        printAndLog "Max tries reached aborting"
+        exit 1
+    fi
+
+    eval "$1>>/tmp/provision_verbose.log"
+
+    if [[ $? > 0 ]]; then
+        printAndLog "Error while running $1, retrying in 5 seconds"
+
+        ((++TRIES))
+
+        sleep 5
+
+        tryCommand "$1" "$TRIES"
+    else
+        printAndLog "Finished $1"
+    fi
+}
